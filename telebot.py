d1a30109f88ca42c8d69dfebc638a5f78109c1bf
vo1odya <93946759+vo1odya@users.noreply.github.com>
2022-09-01 11:51:54 +0300

    Update telebot.py
    demo version

--- telebot.py
+++ telebot.py
@@ -1,65 +1,24 @@
 import telebot
-import random
 
-token ="5725173866:AAGuseJn9EpWJZP-BeuDzGQJKCWauHY0PbM"
+bot = telebot.TeleBot('5748626465:AAHAx5GuSCQZTmA0urAZDpYBy-TqM6KTjHI')
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Привет', 'Пока')
 
-my_name = 'Вова'
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
 
-bot = telebot.TeleBot(token)
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFur1jD4ZPWcHrGR5MgWk1piFkBXIUUgAC_AkAAlxN3gJzKIYgv34NxSkE')
 
-RANDOM_TASKS = ["Записаться на курс", "Покорить вершину", "Помыть машину", "Написать письмо"]
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
 
-HELP = """
-/help - показать список доступных команд.
-/add - добавить команду.
-/show - напечатать все добавленные задачи.
-/exit - выход из раздела комманд.
-/random - добавить случайную задачу на дату Сегодня."""
-
-tasks ={}
-
-def add_todo(date, task):
-    if date in tasks:
-        tasks[date].append(task)
-    else:
-        tasks[date] = []
-        tasks[date].append(task)
-
-@bot.message_handler(commands=["help"])
-def help(message):
-     bot.send_message(message.chat.id, HELP)
-
-@bot.message_handler(commands=["add"])
-def add(message):
-    print(message.text)
-    command = message.text.split(maxsplit=2)
-    date = command[1].lower()
-    task = command[2]
-    add_todo(date, task)
-    text = "Задача: " + task + " добавлена на дату " + date
-    bot.send_message(message.chat.id, text)
-
-@bot.message_handler(commands=["random"])
-def random_add(message):
-    date = "сегодня"
-    task = task = random.choice(RANDOM_TASKS)
-    add_todo(date, task)
-    text = "Задача: " + task + " добавлена на дату " + date
-    bot.send_message(message.chat.id, text)
-
-@bot.message_handler(commands=["show", "print"])
-def show(message): #message.text = /print <date>
-    command = message.text.split(maxsplit=1)
-    date = command[1].lower()
-    text = ""
-    if date in tasks:
-        text = date.upper() + "\n"
-        for task in tasks[date]:
-            text = text + "[]" + task + "\n" # <- символ перевода строки (добавляется через +)
-    else:
-        text = "Задач на эту даты нет"
-    bot.send_message(message.chat.id, text)
-
-
-bot.polling(non_stop=True)
-
+bot.polling()
